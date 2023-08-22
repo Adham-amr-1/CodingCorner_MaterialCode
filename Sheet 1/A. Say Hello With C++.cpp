cf499f623759ce352c5975d411c802a4c23655ef
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-08-22 14:33:02 +0300

    Add files via upload

---
+++ Sheet 1/A. Say Hello With C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <bits/stdc++.h>
+#include<iomanip>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	string st;
+	cin >> st;
+	cout << "Hello, " << st;
+	return 0;
+}
