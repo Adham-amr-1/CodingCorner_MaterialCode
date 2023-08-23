ebf2a8756167a70690b80ebc554763e329e8b596
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-08-23 12:44:55 +0300

    Update Weird Algorithm.cpp

--- Weird Algorithm.cpp
+++ Weird Algorithm.cpp
@@ -7,7 +7,13 @@ int main()
 	cin >> n;
 	while (n != 1) {
 		cout << n << " ";
-		(n % 2 == 0) ? n /= 2 : n = (n * 3) + 1;
+		//(n % 2 == 0) ? n /= 2 : n = (n * 3) + 1;
+		if (n % 2 == 0) {
+			n /= 2;
+		}
+		else {
+			n=(n*3)+1;
+		}
 	}
 	cout << n;
 	return 0;
